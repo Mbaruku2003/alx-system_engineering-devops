206f62258ed1795c2f8a07ff35572317814aa387
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-06 12:12:00 -0700

    102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -9,14 +9,14 @@
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 /**
- * main: creates zombie processes
+ * main - creates zombie processes
  * Return: Always 0
  */
 int main(void)
@@ -34,7 +34,7 @@ int main(void)
 			counting++;
 		}
 		else
-			exit (0);
+			exit(0);
 	}
 	infinite_while();
 	return (EXIT_SUCCESS);
