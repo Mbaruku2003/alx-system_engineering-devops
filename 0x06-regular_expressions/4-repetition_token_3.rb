5e3f503184f860f65fdeb5703d5d3523b72717d9
Mbaruku2003 <leonardjembe21@gmail.com>
2024-05-07 05:22:40 -0700

    4-repetition_token_3.rb

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
 #script that accepts one argument and pass it to a regular expression matching method
-#Your regex should not contain square brackets
 puts ARG[0].scan(/hbt*n/).join
