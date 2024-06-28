807987d83203d67906b54404b8fbf223d1d17d17
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-28 05:41:32 -0700

    we used content t put the contnts to be contained inthe file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#create a file in /tmp
+file { 'create a file in /tmp':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
