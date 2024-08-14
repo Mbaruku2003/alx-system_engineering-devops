d7efa38d24701e75430a2fcd78079f03ec12e71b
Mbaruku2003 <leonardjembe21@gmail.com>
2024-08-14 05:23:57 -0700

    updated a file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """Query subscribers of a given reddit subreddit"""
 import requests
+import json
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a subreddit."""
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    url = f"https:www.reddit.com/r/{subreddit}/about.json"
+
+    header = {'User-Agent': 'Python:subscribers-check:v1.0 (b /u/yourusername)'
             }
-    response = requests.get(url, headers=headers, allow_redirect=False)
-    if response.status_code == 404:
+    try:
+        response = rquests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-    results = response.json().get('data')
-    return results.get("subscribers")
