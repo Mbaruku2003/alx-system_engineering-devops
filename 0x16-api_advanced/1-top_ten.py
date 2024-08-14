7bb167d1bc2aea3765fca91d714de91e45c2d9ca
Mbaruku2003 <leonardjembe21@gmail.com>
2024-08-14 08:24:05 -0700

    updated a file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""queries the Reddit API and prints the titles of the first 10 hot posts listed."""
+"""prints the titles of the first 10 hot posts listed."""
 import requests
 
 
 def top_ten(subreddit):
     """reddit api endpost"""
 
-    url= f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Mozilla/5.0'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
