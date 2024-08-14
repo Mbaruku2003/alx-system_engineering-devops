c48a8434191a6dbfc2b29b94423ae9faac8a1014
Mbaruku2003 <leonardjembe21@gmail.com>
2024-08-14 09:00:32 -0700

    updated a file

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""uses Reddit API to get all posts."""
 import requests
+"""detailed info."""
 
 
 def recurse(subreddit, hot_list=[]):
-    """get all the hot posts."""
-
-    if after is None:
-        return []
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    url += f"?limit=100&after={after}"
-    headers = {'UserAgent': 'Request'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    headers = {'User-Agent': 'Mozilla/5-0'}
+    parameters = {'limit': 100}
+    if after:
+        parameters['after'] = after
+    try:
+        response = requests.get(
+                   url, headers=headers, params=params, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get('data', {}).get('children', [])
+            after = data.get('data', {}).get('after', None)
+            for post in posts:
+                hottest_list.append(post['data']['title'])
+            if after:
+                return recurse(subreddit, hottest_list, after)
+            else:
+                return hottest_list
+        else:
+            return None
+
+    except requests.RequestException:
         return None
-    thejson = response.json()
-    json_post = thejson.get("data").get("children")
-    for post in json_post:
-        the_list.append(post.get("data").get("title"))
-    return the_list + recurse(subreddit, [], thejson.get("data").get("after"))
