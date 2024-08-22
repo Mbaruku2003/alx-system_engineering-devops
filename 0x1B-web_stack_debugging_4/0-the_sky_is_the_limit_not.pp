7c3e9d6d71e083cd540ffcecd1ab54fd86623fa7
Mbaruku2003 <leonardjembe21@gmail.com>
2024-08-22 10:39:57 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # increase the amount of traffic a server can handle
 # increase the unlimit of the default file
 exec { 'modify limit to 4096':
-   command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
-   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+command =>'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+path    =>'/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
