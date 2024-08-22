d8665ddd89c42aa02feada6a10f4cfbfae6bc4a3
Mbaruku2003 <leonardembe21@gmail.com>
2024-08-22 11:11:20 +0000

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,11 +2,11 @@
 # and open a file without any error message.
 #increase the hard file limit for a holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
-   command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
-   path    => '/usr/local/bin/:/bin/'
+command => "sed -i '/holberton hard/s/5/50000/' /etc/security/limits.conf",
+path    => '/usr/local/bin/:/bin/'
 }
 #increase soft file limit for holberton user 
 exec { 'increase-soft-file-limit-for-holberton-user':
-   command => 'sed -i "/^holberton sft/s/4/50000/" /etc/security/limits.conf',
-   path    => '/usr/local/bin/:/bin/'
+command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+path    => '/usr/local/bin/:/bin/'
 }
