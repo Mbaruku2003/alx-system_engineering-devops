d8c87748f71edaf6e896b805e27eadb4376e7faf
Mbaruku2003 <leonardjembe21@gmail.com>
2024-08-22 09:40:32 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,6 @@
-#!/usr/bin/env bash
 # increase the amount of traffic a server can handle
 # increase the unlimit of the default file
-exec { 'fix--for--nginx':
-   #specify the unlimit value
-   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-   #specify the path for the sed command
-   path    => '/usr/local/bin/:/bin/',
-}
-# restart nginx
-exec { 'nginx-restart':
-   command => '/etc/init.d/nginx restart',
-   #specify the path for init.d server
-   path    => '/etc/init.d/',
+exec { 'modify limit to 4096':
+   command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
